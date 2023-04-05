e985c3ad295fad66ad2906a2767780210e7e5279
Govind Kumar <govindkumar@drreddys.com>
2023-04-05 20:52:58 +0530

    gkn portfolio

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } 
+from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import HeroSection from './components/HeroSection'
+
+export default function App() {
+  return (
+    <>
+    <Router>
+      <NavMenu />
+      <HeroSection /> 
+      <Switch>
+        <Route path="/about">
+        <About />
+        </Route>
+        <Route path="/projects">
+        <Projects />
+        </Route>
+        <Route path="/contact">
+        <Contact />
+        </Route>
+        <Route path="/">
+        <Home />
+        </Route>
+      </Switch>
+    </Router>
+    </>
+  );
+}
